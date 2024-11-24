715aad8c251373525d102a580332eb8d8e9f42fd
Marina Lacambra <marina.lacambra@gmail.com>
2024-11-24 20:28:49 +0100

    Initial commit

---
+++ race_PapereEdition_bot.py
@@ -0,0 +1,51 @@
+import discord
+from discord.ext import commands
+import pandas as pd
+import random  # Import the random module
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # This will load the environment variables from .env.txt
+
+token = os.getenv('DISCORD_TOKEN')  # Get the token from the environment
+
+
+# Set up intents
+intents = discord.Intents.default()
+intents.message_content = True  # Enables access to message content
+
+# Set up the bot
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+# Duck GIFs for top performers
+duck_gifs = [
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExaGMwMTZnbndkNmw3ODFndjkxa2c3amNkamMybWRkOWI2ZDllZGV0NCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/RQ7rwyOeftc5z2HTyc/giphy.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExYTBpMzVpaWlseWdmYmt5c3RpZDJ4YnU4ZzhmY29qcWdqZGU0NG5kZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l2Je1eIySBj8c94ly/giphy.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExM3Fjb2R5MWlkdjl0YWEzdXZsbHZ2amVhdndhN2xwanNrYjk5a3lxMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o6MbaZ4f6650Xv3kA/giphy.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExajZmczFqcXNjbm0xZXpkMDZjMDU1d2Zvd3AxeG0yYnNqMXlyZWc5NyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o6MbkfI3ERA6yTqPm/giphy.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNTJpOGM3NDh1Zzg5dXBpd2tjaHpjZmN0ZWphMmpvNGt3cGg5MnI4biZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xUOwG3MyaiItznFs6k/giphy-downsized-large.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExY2RkczQ2OHlrdTBtZWN0cHQzOWh5eWtnOGpmejhxbmp3eTEzdDduaCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/WYPP1DQ7pnXBUWXdRw/giphy-downsized-large.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExcGJjNzZmeGFtamN3NTBjbm9iOHMxd2diZ3IyNGI1bjRvY2lnZzA2YiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/7OxMSjThgSRgsHXOFc/giphy.gif"
+]
+
+# Command to upload the ranking
+@bot.command()
+async def rankingPapere(ctx):
+    try:
+        # Load the CSV and format the ranking message
+        df = pd.read_csv(r'C:\Users\marin\race\output100.csv')
+        ranking_message = "🦆 **Race Papere Edition** 🦆\n\n"
+        for index, row in df.iterrows():
+            gif = random.choice(duck_gifs)  # Select a random GIF
+            ranking_message += f"{index + 1}. {row['Player']} - {row['Total Sum']} points\n"
+            ranking_message += f"{gif}\n"  # Add the GIF to the message
+
+        # Send the ranking message
+        await ctx.send(ranking_message)
+    except Exception as e:
+        await ctx.send("Oops! Something went wrong!")
+        print(e)
+
+# Run the bot
+bot.run(token)  # Use the token securely
+
